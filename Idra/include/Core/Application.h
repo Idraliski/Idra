0c296b43118767bd91eb7d8a85912cee3e5f3ed1
Alan <87167180+Idraliski@users.noreply.github.com>
2025-04-02 09:34:58 +0800

    glfw callbacks to window events

--- Idra/include/Core/Application.h
+++ Idra/include/Core/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Window.h"
+#include "../Events/ApplicationEvent.h"
 
 namespace Idra {
 
@@ -10,8 +11,13 @@ namespace Idra {
 	public:
 		Application();
 		~Application();
+
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
--- Idra/src/Core/Application.cpp
+++ Idra/src/Core/Application.cpp
@@ -1,16 +1,17 @@
 #include "IdraPCH.h"
 #include "Core/Application.h"
 
-#include "Events/ApplicationEvent.h" // #DEBUG
-
 #include <glad/glad.h>
 
 namespace Idra {
+
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1) // #DEBUG
 
 	Application::Application()
 	{
 		IDRA_WARN("Application Created"); // #DEBUG
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
@@ -21,8 +22,6 @@ namespace Idra {
 	void Application::Run()
 	{
 		IDRA_INFO("Application Running"); // #DEBUG
-		WindowResizeEvent e(1280, 720); // #DEBUG
-		IDRA_TRACE(e); // #DEBUG
 
 		while (m_Running)
 		{
@@ -30,6 +29,20 @@ namespace Idra {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+
+		IDRA_TRACE("{0}", e); // #DEBUG
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 } // namespace Idra
