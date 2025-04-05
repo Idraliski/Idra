cd8816871cba199827512e49f850150dfcefea96
Alan <87167180+Idraliski@users.noreply.github.com>
2025-04-05 15:11:58 +0800

    Key and Mouse Codes, copied from glfw.h

---
+++ Idra/include/Core/KeyCodes.h
@@ -0,0 +1,127 @@
+#pragma once
+
+// from glfw3.h
+#define IDRA_KEY_SPACE              32
+#define IDRA_KEY_APOSTROPHE         39  /* ' */
+#define IDRA_KEY_COMMA              44  /* , */
+#define IDRA_KEY_MINUS              45  /* - */
+#define IDRA_KEY_PERIOD             46  /* . */
+#define IDRA_KEY_SLASH              47  /* / */
+#define IDRA_KEY_0                  48
+#define IDRA_KEY_1                  49
+#define IDRA_KEY_2                  50
+#define IDRA_KEY_3                  51
+#define IDRA_KEY_4                  52
+#define IDRA_KEY_5                  53
+#define IDRA_KEY_6                  54
+#define IDRA_KEY_7                  55
+#define IDRA_KEY_8                  56
+#define IDRA_KEY_9                  57
+#define IDRA_KEY_SEMICOLON          59  /* ; */
+#define IDRA_KEY_EQUAL              61  /* = */
+#define IDRA_KEY_A                  65
+#define IDRA_KEY_B                  66
+#define IDRA_KEY_C                  67
+#define IDRA_KEY_D                  68
+#define IDRA_KEY_E                  69
+#define IDRA_KEY_F                  70
+#define IDRA_KEY_G                  71
+#define IDRA_KEY_H                  72
+#define IDRA_KEY_I                  73
+#define IDRA_KEY_J                  74
+#define IDRA_KEY_K                  75
+#define IDRA_KEY_L                  76
+#define IDRA_KEY_M                  77
+#define IDRA_KEY_N                  78
+#define IDRA_KEY_O                  79
+#define IDRA_KEY_P                  80
+#define IDRA_KEY_Q                  81
+#define IDRA_KEY_R                  82
+#define IDRA_KEY_S                  83
+#define IDRA_KEY_T                  84
+#define IDRA_KEY_U                  85
+#define IDRA_KEY_V                  86
+#define IDRA_KEY_W                  87
+#define IDRA_KEY_X                  88
+#define IDRA_KEY_Y                  89
+#define IDRA_KEY_Z                  90
+#define IDRA_KEY_LEFT_BRACKET       91  /* [ */
+#define IDRA_KEY_BACKSLASH          92  /* \ */
+#define IDRA_KEY_RIGHT_BRACKET      93  /* ] */
+#define IDRA_KEY_GRAVE_ACCENT       96  /* ` */
+#define IDRA_KEY_WORLD_1            161 /* non-US #1 */
+#define IDRA_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define IDRA_KEY_ESCAPE             256
+#define IDRA_KEY_ENTER              257
+#define IDRA_KEY_TAB                258
+#define IDRA_KEY_BACKSPACE          259
+#define IDRA_KEY_INSERT             260
+#define IDRA_KEY_DELETE             261
+#define IDRA_KEY_RIGHT              262
+#define IDRA_KEY_LEFT               263
+#define IDRA_KEY_DOWN               264
+#define IDRA_KEY_UP                 265
+#define IDRA_KEY_PAGE_UP            266
+#define IDRA_KEY_PAGE_DOWN          267
+#define IDRA_KEY_HOME               268
+#define IDRA_KEY_END                269
+#define IDRA_KEY_CAPS_LOCK          280
+#define IDRA_KEY_SCROLL_LOCK        281
+#define IDRA_KEY_NUM_LOCK           282
+#define IDRA_KEY_PRINT_SCREEN       283
+#define IDRA_KEY_PAUSE              284
+#define IDRA_KEY_F1                 290
+#define IDRA_KEY_F2                 291
+#define IDRA_KEY_F3                 292
+#define IDRA_KEY_F4                 293
+#define IDRA_KEY_F5                 294
+#define IDRA_KEY_F6                 295
+#define IDRA_KEY_F7                 296
+#define IDRA_KEY_F8                 297
+#define IDRA_KEY_F9                 298
+#define IDRA_KEY_F10                299
+#define IDRA_KEY_F11                300
+#define IDRA_KEY_F12                301
+#define IDRA_KEY_F13                302
+#define IDRA_KEY_F14                303
+#define IDRA_KEY_F15                304
+#define IDRA_KEY_F16                305
+#define IDRA_KEY_F17                306
+#define IDRA_KEY_F18                307
+#define IDRA_KEY_F19                308
+#define IDRA_KEY_F20                309
+#define IDRA_KEY_F21                310
+#define IDRA_KEY_F22                311
+#define IDRA_KEY_F23                312
+#define IDRA_KEY_F24                313
+#define IDRA_KEY_F25                314
+#define IDRA_KEY_KP_0               320
+#define IDRA_KEY_KP_1               321
+#define IDRA_KEY_KP_2               322
+#define IDRA_KEY_KP_3               323
+#define IDRA_KEY_KP_4               324
+#define IDRA_KEY_KP_5               325
+#define IDRA_KEY_KP_6               326
+#define IDRA_KEY_KP_7               327
+#define IDRA_KEY_KP_8               328
+#define IDRA_KEY_KP_9               329
+#define IDRA_KEY_KP_DECIMAL         330
+#define IDRA_KEY_KP_DIVIDE          331
+#define IDRA_KEY_KP_MULTIPLY        332
+#define IDRA_KEY_KP_SUBTRACT        333
+#define IDRA_KEY_KP_ADD             334
+#define IDRA_KEY_KP_ENTER           335
+#define IDRA_KEY_KP_EQUAL           336
+#define IDRA_KEY_LEFT_SHIFT         340
+#define IDRA_KEY_LEFT_CONTROL       341
+#define IDRA_KEY_LEFT_ALT           342
+#define IDRA_KEY_LEFT_SUPER         343
+#define IDRA_KEY_RIGHT_SHIFT        344
+#define IDRA_KEY_RIGHT_CONTROL      345
+#define IDRA_KEY_RIGHT_ALT          346
+#define IDRA_KEY_RIGHT_SUPER        347
+#define IDRA_KEY_MENU               348
+
+#define IDRA_KEY_LAST               IDRA_KEY_MENU
---
+++ Idra/include/Core/MouseButtonCodes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// from glfw3.h
+#define IDRA_MOUSE_BUTTON_1         0
+#define IDRA_MOUSE_BUTTON_2         1
+#define IDRA_MOUSE_BUTTON_3         2
+#define IDRA_MOUSE_BUTTON_4         3
+#define IDRA_MOUSE_BUTTON_5         4
+#define IDRA_MOUSE_BUTTON_6         5
+#define IDRA_MOUSE_BUTTON_7         6
+#define IDRA_MOUSE_BUTTON_8         7
+#define IDRA_MOUSE_BUTTON_LAST      IDRA_MOUSE_BUTTON_8
+#define IDRA_MOUSE_BUTTON_LEFT      IDRA_MOUSE_BUTTON_1
+#define IDRA_MOUSE_BUTTON_RIGHT     IDRA_MOUSE_BUTTON_2
+#define IDRA_MOUSE_BUTTON_MIDDLE    IDRA_MOUSE_BUTTON_3
