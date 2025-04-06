8b11003e429f83c3989d548760f74ef53cef832e
Alan <87167180+Idraliski@users.noreply.github.com>
2025-04-06 13:19:02 +0800

    Rendering a new viewport for ImGui

--- Idra/src/ImGui/ImGuiLayer.cpp
+++ Idra/src/ImGui/ImGuiLayer.cpp
@@ -33,8 +33,8 @@ namespace Idra
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
 		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-		//io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons; // Disable taskbar icons on secondary windows
 		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;     // Disable merging multiple windows into a single one
 
@@ -45,7 +45,7 @@ namespace Idra
 
 		// when viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones
 		ImGuiStyle& style = ImGui::GetStyle();
-		//if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
 			style.WindowRounding = 0.0f;
 			style.Colors[ImGuiCol_WindowBg].w = 1.0f; // Set alpha to opaque
@@ -90,25 +90,15 @@ namespace Idra
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		/*
 		// When using multiple viewports: platform windows are drawn into their own framebuffer
 		// and need to be presented explicitly. For this we use the ImGuiPlatformIO::RenderWindow function.
-		//if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
 			GLFWwindow* backup_current_context = glfwGetCurrentContext();
-			ImGuiPlatformIO& platform_io = ImGui::GetPlatformIO();
-			for (size_t i = 0; i < platform_io.Viewports.Size; i++)
-			{
-				ImGuiViewport* viewport = platform_io.Viewports[i];
-				glfwMakeContextCurrent((GLFWwindow*)viewport->PlatformHandle);
-				glViewport(0, 0, (int)viewport->Size.x, (int)viewport->Size.y);
-				glClearColor(0.1f, 0.105f, 0.11f, 1.0f);
-				glClear(GL_COLOR_BUFFER_BIT);
-				//platform_io.RenderWindow(viewport);
-			}
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
 			glfwMakeContextCurrent(backup_current_context);
 		}
-		*/
 	}
 
 	void ImGuiLayer::SetDarkThemeColors()
