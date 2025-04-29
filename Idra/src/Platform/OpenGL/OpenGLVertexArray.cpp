033b54a4f797768c32e4452423bf1604786acd16
Alan <87167180+Idraliski@users.noreply.github.com>
2025-04-29 09:53:00 +0800

    formatting

--- Idra/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Idra/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -10,18 +10,19 @@ namespace Idra {
 	{
 		switch (type)
 		{
-		case ShaderDataType::Float:   return GL_FLOAT;
-		case ShaderDataType::Float2:  return GL_FLOAT;
-		case ShaderDataType::Float3:  return GL_FLOAT;
-		case ShaderDataType::Float4:  return GL_FLOAT;
-		case ShaderDataType::Mat3:    return GL_FLOAT;
-		case ShaderDataType::Mat4:    return GL_FLOAT;
-		case ShaderDataType::Int:     return GL_INT;
-		case ShaderDataType::Int2:    return GL_INT;
-		case ShaderDataType::Int3:    return GL_INT;
-		case ShaderDataType::Int4:    return GL_INT;
-		case ShaderDataType::Bool:    return GL_BOOL;
+			case ShaderDataType::Float:   return GL_FLOAT;
+			case ShaderDataType::Float2:  return GL_FLOAT;
+			case ShaderDataType::Float3:  return GL_FLOAT;
+			case ShaderDataType::Float4:  return GL_FLOAT;
+			case ShaderDataType::Mat3:    return GL_FLOAT;
+			case ShaderDataType::Mat4:    return GL_FLOAT;
+			case ShaderDataType::Int:     return GL_INT;
+			case ShaderDataType::Int2:    return GL_INT;
+			case ShaderDataType::Int3:    return GL_INT;
+			case ShaderDataType::Int4:    return GL_INT;
+			case ShaderDataType::Bool:    return GL_BOOL;
 		}
+
 		IDRA_CORE_ASSERT(false, "Unknown shader data type!");
 		return 0;
 	}
