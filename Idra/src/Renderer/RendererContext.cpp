949aa2ab335eaa41be2af7f2704c6211ef775617
Alan <87167180+Idraliski@users.noreply.github.com>
2025-05-03 11:15:20 +0800

    added a todo comment

--- Idra/src/Renderer/RendererContext.cpp
+++ Idra/src/Renderer/RendererContext.cpp
@@ -9,6 +9,7 @@
 namespace Idra {
 	RenderingContext* RenderingContext::Create(GLFWwindow* windowHandle)
 	{
+		//@TODO: Some work will need to be done to refactor this to use a switch on the enum of RendererAPI
 		return new OpenGLContext(windowHandle);
 	}
 }
