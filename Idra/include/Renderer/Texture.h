d1fa8dbe64fb7a7d5ee2a2d265c719f58bbfb5ef
Alan <87167180+Idraliski@users.noreply.github.com>
2025-06-06 09:04:58 +0800

    Dockspace Init

--- Idra/include/Renderer/Texture.h
+++ Idra/include/Renderer/Texture.h
@@ -14,6 +14,7 @@ namespace Idra {
 
 		virtual void Bind(uint32_t slot = 0) const = 0;
 		virtual void Unbind(uint32_t slot = 0) const = 0;
+		virtual uint32_t GetRendererID() const = 0;
 
 		virtual uint32_t GetWidth() const = 0;
 		virtual uint32_t GetHeight() const = 0;
